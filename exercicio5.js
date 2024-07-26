8f6a1d8318bf9eb17075a8e2bda01c632da517ec
Fernando Kitakawa <ferkitakawa123@gmail.com>
2024-07-25 21:17:37 -0300

    Exercício para calcular a tabuada do número informado pelo usuário.

---
+++ exercicio5.js
@@ -0,0 +1,16 @@
+/* Faça um algoritmo que calcule e mostre a tabuada de um número digitado pelo usuário.*/
+
+const readline = require("readline-sync");
+
+let n1 = parseInt(readline.question("Informe o número para apresentar a respectiva tabuada :"))
+
+function calcularTabuada(n1){
+
+    let resultado = 0
+
+    for(let i = 0 ; i <= 10 ; i++){
+        resultado = n1 * i
+        console.log(`${n1} x ${i} = ${resultado} `)
+    }
+}
+calcularTabuada(n1)
