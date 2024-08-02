71cbc407f7820cabc2d767bb48e465fbe2d604a6
Fernando Kitakawa <ferkitakawa123@gmail.com>
2024-08-01 22:20:28 -0300

    Merge Sort - Início da implementação

---
+++ exercicio10.js
@@ -0,0 +1,13 @@
+/*Algoritmo Merge Sort. Implementação e resolução.*/
+
+let arraynumero = [30,20,50,40]
+
+function divisaoArray(arraynumero){
+    let meio = math.floor(arraynumero.length/2)
+    let esquerda = arraynumero.slice(0,meio)
+    let direita = arraynumero.slice(meio)
+}
+
+function ordenacao(arraynumero){
+    
+}
