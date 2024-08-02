1ce9d5e4bd360769f1ddd480e732f74fd5bc69cc
Fernando Kitakawa <ferkitakawa123@gmail.com>
2024-08-01 22:27:11 -0300

    Merge Sort- Término do método de dividir sub-arrays

--- exercicio10.js
+++ exercicio10.js
@@ -3,11 +3,17 @@
 let arraynumero = [30,20,50,40]
 
 function divisaoArray(arraynumero){
+    if(arraynumero.length <= 1 ){
+        return arraynumero
+    }
+
     let meio = math.floor(arraynumero.length/2)
     let esquerda = arraynumero.slice(0,meio)
     let direita = arraynumero.slice(meio)
+
+    ordenacao(esquerda, direita)
 }
 
-function ordenacao(arraynumero){
+function ordenacao(arraynumero, esquerda, direita){
     
 }
