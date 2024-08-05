3076b87809b847a4bf09abcc0eeee82b702a52d8
Fernando Kitakawa <ferkitakawa123@gmail.com>
2024-08-04 23:46:47 -0300

    Criando testes respectivamente para o exercício 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,6 +1,14 @@
 /*Construa um algoritmo que, tendo como dados de entrada dois pontos quaisquer no plano, P(x1,y1) e P(x2,y2), escreva a distância entre eles.*/
 
 let x1 = 2, y1 = 3, x2 = 4 , y2 = 6
-const d = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2)); //Fórmula da distância que irá definir o resultado
 
-console.log(`A distância entre os pontos ${x1},${y1} , ${x2}, ${y2} é ${d.toFixed(2)}`)
+function calcularDistancia(x1,y1,x2,y2){
+const d = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2)); //Fórmula da distância que irá definir o resultado
+return d
+}
+
+let result = calcularDistancia(x1,y1,x2,y2)
+
+console.log(`A distância entre os pontos ${x1},${y1} , ${x2}, ${y2} é ${result.toFixed(2)}`)
+
+module.export = calcularDistancia
---
+++ testes/exercicio1.test.js
@@ -0,0 +1,4 @@
+import calcularDistancia from "../exercicio1";
+
+
+
